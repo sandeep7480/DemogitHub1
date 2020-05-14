c7361c749e9c3b2d5a6f764c7a52fd5b5b26e7e7
sandeepdhote <sandeepdhote@192.168.1.102>
2020-05-14 08:29:30 +0530

    only java program has been changed

--- WorkSpace2/DemoGitHubWP2/src/com/sampleone/Demoprogrameon1.java
+++ WorkSpace2/DemoGitHubWP2/src/com/sampleone/Demoprogrameon1.java
@@ -4,7 +4,7 @@ public class Demoprogrameon1 {
 	public static void main(String[] args) {
 		System.out.println("1st data");
 		System.out.println("2nd data");
-		
+		System.out.println("3rd data on 8:30AM on 14May");
 	}
 
 	}
